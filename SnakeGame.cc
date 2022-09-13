373cbfcb769c6e4a695cadd315c4fb4d8936222a
Kevin Thalmann <96835756+KevinThalmann222@users.noreply.github.com>
2022-09-13 13:26:49 +0200

    Adding  Snakehead, Fruit and Score
    - the function "GameArea" is performed in a loop
    - the fruit is generated at a random location in the aero
    - the snakehead is in the middle of the area

--- SnakeGame.cc
+++ SnakeGame.cc
@@ -1,12 +1,27 @@
 #include <iostream>
+#include <stdlib.h>
+#include <windows.h>
 using namespace std;
 
 const int height = 30;
 const int width = 60;
 bool gameover = false;
+int score = 0;
+int x_fruit, y_fruit;
+int x_snake = width / 2;
+int y_snake = height / 2;
+
+void GameSetup()
+{
+    // random number in range 0 to width-1
+    x_fruit = rand() % width;
+    // random number in range 0 to height-1
+    y_fruit = rand() % height;
+}
 
 void GameArea()
 {
+    system("clear");
     // Ausgabe der oberen Reihe
     for (int x = 0; x < width; x++)
     {
@@ -22,6 +37,14 @@ void GameArea()
             {
                 cout << "|";
             }
+            else if (x == x_snake && y == y_snake)
+            {
+                cout << "O";
+            }
+            else if (x == x_fruit && y == y_fruit)
+            {
+                cout << "F";
+            }
             else
             {
                 cout << " ";
@@ -35,10 +58,18 @@ void GameArea()
         cout << "-";
     }
     cout << endl;
+    cout << "Your Score is: " << score << endl;
 }
 
 int main()
 {
-    GameArea();
+    GameSetup();
+    while (!gameover)
+    {
+        GameArea();
+        // 50ms wird gewartet
+        Sleep(50);
+    }
+
     return 0;
 }
